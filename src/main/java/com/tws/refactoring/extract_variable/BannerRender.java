5862c89ed143db226095190158874bc2ad299522
1134923982 <1134923982@qq.com>
2019-07-15 19:03:25 +0800

    [refactor:BannerRender]

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,8 +2,9 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     String renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
+        final boolean doesPlatformContainMAC = platform.toUpperCase().contains("MAC");
+        final boolean doesBrowserContainIE = browser.toUpperCase().contains("IE");
+        if (doesPlatformContainMAC && doesBrowserContainIE) {
             return "IE on Mac?";
         }
         return "banner";
