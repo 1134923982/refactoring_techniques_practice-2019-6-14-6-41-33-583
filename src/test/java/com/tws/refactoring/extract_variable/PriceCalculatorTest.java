1f2717ef4fd2d8d6a4cf9fcbd2ba234a9578bb52
1134923982 <1134923982@qq.com>
2019-07-15 19:12:22 +0800

    [test:PriceCalculatorTest]

--- src/test/java/com/tws/refactoring/extract_variable/PriceCalculatorTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/PriceCalculatorTest.java
@@ -5,34 +5,34 @@ import org.junit.Test;
 
 public class PriceCalculatorTest {
     @Test
-    public void should_return__when_give_10_and_50(){
+    public void should_return_550_when_give_10_and_50(){
         PriceCalculator priceCalculator = new PriceCalculator();
         double price = priceCalculator.getPrice(10,50);
         Assert.assertEquals(550,price,0);
     }
 
     @Test
-    public void should_return__when_give_500_and_50(){
+    public void should_return_25100_when_give_500_and_50(){
         PriceCalculator priceCalculator = new PriceCalculator();
         double price = priceCalculator.getPrice(500,50);
         Assert.assertEquals(25100.0,price,0);
     }
     @Test
-    public void should_return__when_give_501_and_50(){
+    public void should_return_25147_when_give_501_and_50(){
         PriceCalculator priceCalculator = new PriceCalculator();
         double price = priceCalculator.getPrice(501,50);
         Assert.assertEquals(25147.5,price,0);
     }
 
     @Test
-    public void should_return__when_give_100_and_10(){
+    public void should_return_1100_when_give_100_and_10(){
         PriceCalculator priceCalculator = new PriceCalculator();
         double price = priceCalculator.getPrice(100,10);
         Assert.assertEquals(1100.0,price,0);
     }
 
     @Test
-    public void should_return__when_give_101_and_10(){
+    public void should_return_1110_when_give_101_and_10(){
         PriceCalculator priceCalculator = new PriceCalculator();
         double price = priceCalculator.getPrice(101,10);
         Assert.assertEquals(1110.0,price,0);
